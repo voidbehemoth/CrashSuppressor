e2b536cb4f1d6d3be0abb1ac75923d2531da9add
VoidBehemoth <VoidBehemoth@gmail.com>
2023-10-15 20:10:57 -0600

    Add project files.

---
+++ CrashSuppressor/src/CrashSuppressor.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Home.HomeScene;
+using SML;
+using UnityEditor.CrashReporting;
+
+namespace CrashSuppressor
+{
+    [Mod.SalemMod]
+    public class Main
+    {
+        public static void Start()
+        {
+            Utils.Logger.Log("ain't no way");
+        }
+    }
+
+    [HarmonyPatch(typeof(HomeSceneController), "Start")]
+    public class CrashReportSuppressor
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            CrashReportingSettings.enabled = false;
+        }
+    }
+
+    public static class ModInfo
+    {
+        public const string PLUGIN_GUID = "CrashSuppressor";
+
+        public const string PLUGIN_NAME = "CrashSuppressor";
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
---
+++ CrashSuppressor/src/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrashSuppressor.Utils
+{
+    public class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine("[" + CrashSuppressor.ModInfo.PLUGIN_GUID + "] " + message);
+        }
+    }
+}
